76c740bc7c84f399827fe31e132af8aba22c3676
BhavanaGaddam <bhavana.gaddam00@gmail.com>
2021-05-25 09:31:57 +0530

    Update model.py

--- python-flask/model.py
+++ python-flask/model.py
@@ -16,18 +16,12 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from scipy.stats import chi2_contingency
+from sklearn.model_selection import StratifiedKFold
 
 """ PART 2
 Importing Data """
 df = pd.read_csv('diabetes.csv')
-#print ("Data Head : \n", df.head())
-#print ("\nData Tail : \n", df.tail())
-#print("\nSample : \n", df.sample(5))
-#print("\nData Types:\n",df.dtypes)#datatypes
-#print("\nData Info:\n",df.info())
-#print("\nData Shape:\n",df.shape)#dimensions
-#print ("\n\nData Describe : \n", df.describe())#statistical summary
-#summary=df.describe(include='all')
+
 """ PART 3
 Checking Null Values """
 #for c in summary.columns:
@@ -35,10 +29,6 @@ Checking Null Values """
     
 """ PART 4
 Remove Outliers """
-#for i in df:
-#    if df[i].dtype=='int64' and i!='Outcome':
-#        df[i]=df[i].replace(0,df[i].mean())
-        
 def remove_outlier(df_in, col_name):
     q1 = df_in[col_name].quantile(0.25)
     q3 = df_in[col_name].quantile(0.75)
@@ -57,214 +47,27 @@ df=remove_outlier(df,'SkinThickness')
 upper = df.corr().where(np.triu(np.ones(df.corr().shape),
                                       k=1).astype(np.bool))
 to_drop = [column for column in upper.columns if any(upper[column] > 0.97)]
-#print(to_drop)
 df = df.drop(df[to_drop], axis=1)
-#print(df.columns)
-#print(sns.boxplot(x=df["SkinThickness"]))
-#plt.figure(figsize=(20,5))
-#sns.heatmap(df.corr(),annot=True,cmap='YlGnBu', vmin=None, vmax=None,linewidths=0)
-#df=df.drop(['Age'],axis=1)
-#print(df)
-#print(sns.boxplot(x=df["Insulin"]))
 df['Age']=pd.cut(df['Age'],3,labels=['young','middle','old'])
 df['Age'] = df['Age'].map({"young":0,"middle":1,"old":2})
-#print(df)
 df['Glucose']=pd.cut(df['Glucose'],3,labels=['low','medium','high'])
 df['Glucose'] = df['Glucose'].map({"low":0,"medium":1,"high":2})
-#print(df)
 df['BloodPressure']=pd.cut(df['BloodPressure'],3,labels=['low','medium','high'])
 df['BloodPressure'] = df['BloodPressure'].map({"low":0,"medium":1,"high":2})
-#print(df)
 df['Insulin']=pd.cut(df['Insulin'],3,labels=['low','medium','high'])
 df['Insulin'] = df['Insulin'].map({"low":0,"medium":1,"high":2})
-#print(df)
 df['BMI']=pd.cut(df['BMI'],3,labels=['low','medium','high'])
 df['BMI'] = df['BMI'].map({"low":0,"medium":1,"high":2})
-#print(df)
 df['DiabetesPedigreeFunction']=pd.cut(df['DiabetesPedigreeFunction'],3,labels=['normal','prediabetes','diabetes'])
 df['DiabetesPedigreeFunction'] = df['DiabetesPedigreeFunction'].map({"normal":0,"prediabetes":1,"diabetes":2})
-#print(df)
 y=df.Outcome
 x=df.drop('Outcome',axis=1)
 
-'''
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.1,random_state=100)
-##standard scalar
-st_x=StandardScaler()
-x_train=st_x.fit_transform(x_train)
-x_test=st_x.transform(x_test)
-
-l=[]
-l2=[]
-#print(df.dtypes)
-for i in df:
-    if df[i].dtype=='float64':
-        l2.append(i)
-    else:
-        l.append(i)
-#print(l)
-#print(l2)
-def chi_sqaure(i,j):
-    table=pd.crosstab(i,j) 
-    stat,p,dof,expected=chi2_contingency(table)
-#    print(p)
-    alpha=0.05
-    if p<=alpha:
-        return True
-    else:
-        return False
-for i in l:
-    d=chi_sqaure(df['Outcome'],df[i])
-    if d==False:
-        df=df.drop(i,axis=1)
-#print(df.dtypes)
-        
-##PCA
-pca = PCA()   
-x_train = pca.fit_transform(x_train) 
-x_test = pca.transform(x_test) 
-explained_variance = pca.explained_variance_ratio_
-#print(x_train)
-#print(x_test)
-
-""" PART 5
-Applying Algorithms """
-acc=[]
-
-##Smote test
-sm = SMOTE(random_state = 2) 
-x_train_res, y_train_res = sm.fit_resample(x_train, y_train.ravel())
-
-
-lr1 = LogisticRegression() 
-#lr1.fit(x_train, y_train) 
-lr1.fit(x_train_res, y_train_res) 
-predictions = lr1.predict(x_test) 
-# print classification report 
-#print(classification_report(y_test, predictions)) 
-print("Accuracy percentage LR:"+"{:.2f}".format(accuracy_score(y_test,predictions)*100))
-lr_accuracy_score=accuracy_score(y_test,predictions)*100
-acc.append(round(lr_accuracy_score,2))
-#confusion matrix
-cm = confusion_matrix(y_test, predictions)
-#print(cm)
-# Saving model to disk
-pickle.dump(lr1, open('model.pkl','wb'))
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
-
-
-##SupportVectorClassifier
-S=SVC()
-#Train the model
-S.fit(x_train_res,y_train_res)
-#predicting outcome
-y_pre_S=S.predict(x_test)
-print("Accuracy percentage SVC:"+"{:.2f}".format(accuracy_score(y_test,y_pre_S)*100))
-svc_accuracy_score=accuracy_score(y_test,y_pre_S)*100
-acc.append(round(svc_accuracy_score,2))
-#confusion matrix
-cm = confusion_matrix(y_test, y_pre_S)
-#print(cm)
-# Saving model to disk
-pickle.dump(S, open('model.pkl','wb'))
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
-
-
-##NaiveBayesClassifier
-gnb = GaussianNB()
-#Train the model
-gnb.fit(x_train_res,y_train_res)
-#predicting outcome
-y_pre_gnb=gnb.predict(x_test)
-print("Accuracy percentage GNB:"+"{:.2f}".format(accuracy_score(y_test,y_pre_gnb)*100))
-gnb_accuracy_score=accuracy_score(y_test,y_pre_gnb)*100
-acc.append(round(gnb_accuracy_score,2))
-#confusion matrix
-cm = confusion_matrix(y_test, y_pre_gnb)
-#print(cm)
-# Saving model to disk
-pickle.dump(gnb, open('model.pkl','wb'))
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
-
-
-##DecisionTreeClassifier
-DTC=DecisionTreeClassifier()
-#Train the model
-DTC.fit(x_train_res,y_train_res)
-#predicting outcome
-y_pre_DTC=DTC.predict(x_test)
-print("Accuracy percentage DTC:"+"{:.2f}".format(accuracy_score(y_test,y_pre_DTC)*100))
-dtc_accuracy_score=accuracy_score(y_test,y_pre_DTC)*100
-acc.append(round(dtc_accuracy_score,2))
-#confusion matrix
-cm = confusion_matrix(y_test, y_pre_DTC)
-#print(cm)
-# Saving model to disk
-pickle.dump(DTC, open('model.pkl','wb'))
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
-
-
-##RandomForestClassifier
-RF=RandomForestClassifier()
-#Train the model
-RF.fit(x_train_res,y_train_res)
-#predicting outcome
-y_pre_RF=RF.predict(x_test)
-print("Accuracy percentage RF:"+"{:.2f}".format(accuracy_score(y_test,y_pre_RF)*100))
-rf_accuracy_score=accuracy_score(y_test,y_pre_RF)*100
-acc.append(round(rf_accuracy_score,2))
-#confusion matrix
-cm = confusion_matrix(y_test, y_pre_RF)
-#print(cm)
-# Saving model to disk
-pickle.dump(RF, open('model.pkl','wb'))
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
-
-
-##KNeighborsClassifier
-KNC=KNeighborsClassifier()
-#Train the model
-KNC.fit(x_train_res,y_train_res)
-#predicting outcome
-y_pre_KNC=KNC.predict(x_test)
-print("Accuracy percentage KNC:"+"{:.2f}".format(accuracy_score(y_test,y_pre_KNC)*100))
-knc_accuracy_score=accuracy_score(y_test,y_pre_KNC)*100
-acc.append(round(knc_accuracy_score,2))
-#confusion matrix
-cm = confusion_matrix(y_test, y_pre_KNC)
-#print(cm)
-# Saving model to disk
-pickle.dump(KNC, open('model.pkl','wb'))
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
-
-
-models=['LR','SVC','DTS','RF','KNN','GNB']
-fig = plt.figure()
-import matplotlib
-matplotlib.style.use('ggplot')
-ax = fig.add_axes([0,0,1,1])
-plt.title("Accuracy Score of different models ")
-ax.bar(models,acc,width=0.8,color=['yellow','pink','skyblue','brown','green','orange'])
-
-'''
-from sklearn. model_selection import StratifiedKFold
 accuracy=[]
 skf = StratifiedKFold(n_splits=10, random_state=None)
 skf.get_n_splits(x, y)
 
-# X is the feature set and y is the target
+# x is the feature set and y is the target
 for train_index, test_index in skf.split(x, y) :
     #print ("Train:", train_index, "validation:", test_index)
     X1_train, X1_test = x.iloc[train_index], x.iloc[test_index]
@@ -290,12 +93,9 @@ for train_index, test_index in skf.split(x, y) :
             l2.append(i)
         else:
             l.append(i)
-    #print(l)
-    #print(l2)
     def chi_sqaure(i,j):
         table=pd.crosstab(i,j) 
         stat,p,dof,expected=chi2_contingency(table)
-    #    print(p)
         alpha=0.05
         if p<=alpha:
             return True
@@ -305,12 +105,11 @@ for train_index, test_index in skf.split(x, y) :
         d=chi_sqaure(df['Outcome'],df[i])
         if d==False:
             df=df.drop(i,axis=1)
-    #print(df.dtypes)
-    classifier=LogisticRegression()
-#    classifier=SVC()
+#    classifier=LogisticRegression()
+    classifier=SVC()
 #    classifier=GaussianNB()
 #    classifier=DecisionTreeClassifier()
-#    classifier=RandomForestClassifier(n_estimators = 20, criterion = 'entropy', random_state = 51)
+#    classifier=RandomForestClassifier()
 #    classifier=KNeighborsClassifier()
     classifier.fit(X1_train, y1_train)
     prediction=classifier . predict(X1_test)
@@ -320,5 +119,4 @@ for train_index, test_index in skf.split(x, y) :
     pickle.dump(classifier, open('model.pkl','wb'))
     # Loading model to compare the results
     model = pickle.load(open('model.pkl','rb'))
-    #print(model.predict([[2, 9, 6]]))
-#print ((np.array(accuracy).mean())*100)
+#print((np.array(accuracy).mean())*100)
